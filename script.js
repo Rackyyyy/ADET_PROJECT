1c12b9f4b27d8428e452ba1a36d64fe3bece283e
Red Dolosa <schoolfile1129@gmail.com>
2025-03-28 02:00:52 +0800

    landing page - need to fix

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const orderButton = document.querySelector('.promo-info button');
+    orderButton.addEventListener('click', function() {
+        alert('Order placed successfully!');
+    });
+});
